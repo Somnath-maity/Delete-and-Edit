5d28a5beb31fd2006097448a3eb675abba0d78e3
sravani2s <89511702+sravani2s@users.noreply.github.com>
2022-09-12 21:35:31 +0530

    Added Filter

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 var form = document.getElementById('addForm'); 
-var itemList = document.getElementById('items'); 
+var itemList = document.getElementById('items');  
+var filter = document.getElementById('filter');
 
 // Form submit event 
 form.addEventListener('submit', addItem);  
 // delete event 
-itemList.addEventListener('click', removeItem);
+itemList.addEventListener('click', removeItem); 
+// filter event 
+filter.addEventListener('keyup', filterItems); 
+// // filter event 
+// filter.addEventListener('keyup', filterItem); 
 
 // Add item 
 function addItem(e){
@@ -12,13 +17,14 @@ function addItem(e){
 
     // Get input value 
     var newItem = document.getElementById('item').value; 
-
+    var newItem1 = document.getElementById('itemone').value; 
     // Create new li element 
     var li = document.createElement('li'); 
     // Add class 
     li.className = 'list-group-item'; 
     // Add text node with input value 
     li.appendChild(document.createTextNode(newItem));  
+    li.appendChild(document.createTextNode(newItem1));  
 
 
     // Create delet button element 
@@ -51,4 +57,36 @@ if(e.target.classList.contains('delete')){
         itemList.removeChild(li);
     }
 }
+}   
+
+// Filter Items 
+function filterItems(e) {
+    // convert text to lowercase 
+    var text = e.target.value.toLowerCase(); 
+    console.log(text);  
+
+    // var text1 = e.target.value.toLowerCase(); 
+    // console.log(text1);  
+
+    // Get lis 
+    var items = itemList.getElementsByTagName('li'); 
+    // Convert to an array 
+    Array.from(items).forEach(function(item){
+        var itemName = item.firstChild.textContent; 
+        if(itemName.toLowerCase().indexOf(text)!=-1){
+        item.style.display = 'block'; 
+        } else{
+            item.style.display = 'none';
+        }
+    });  
+
+    // Convert to an array 
+    Array.from(items).forEach(function(itemone){
+        var itemName1 = itemone.childNodes[1].textContent; 
+        if(itemName1.toLowerCase().indexOf(text)!=-1){
+            itemone.style.display = 'block'; 
+        } else{
+            itemone.style.display = 'none';
+        }
+    }); 
 }
